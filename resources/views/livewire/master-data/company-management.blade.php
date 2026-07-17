98eedd39a963f9dfd4e1fdf6fdef1f6f1ad933ea
Dimas Prabowo <dimasprabowo.cool@gmail.com>
2026-07-17 18:21:42 +0700

    update filter in perusahaan

--- resources/views/livewire/master-data/company-management.blade.php
+++ resources/views/livewire/master-data/company-management.blade.php
@@ -162,7 +162,7 @@
                         </tr>
                     @empty
                         <tr>
-                            <td colspan="6" class="px-6 py-12 text-center">
+                            <td colspan="7" class="px-6 py-12 text-center">
                                 <svg class="mx-auto h-12 w-12 text-gray-400" fill="none" stroke="currentColor" viewBox="0 0 24 24">
                                     <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 21V5a2 2 0 00-2-2H7a2 2 0 00-2 2v16m14 0h2m-2 0h-5m-9 0H3m2 0h5M9 7h1m-1 4h1m4-4h1m-1 4h1m-5 10v-5a1 1 0 011-1h2a1 1 0 011 1v5m-4 0h4"/>
                                 </svg>
